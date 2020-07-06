ea9462544936aebdfb967c003887a2ed83cd8425
uelgerni <https://uelgerni@github.com>
2020-07-06 16:35:25 +0200

    dann halt mit normaler suche und mergesort

---
+++ V3.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+"""
+reads filename from directory
+"""
+
+
+def numpyReader(filename):
+    return np.genfromtxt('./data/{}'.format(filename), delimiter=',', dtype=float)
+
+
+def pandasReader(filename):
+    dataframe = pd.read_csv('./data/{}'.format(filename), header=None)
+    names = ["Colour"]
+    for i in range(dataframe.shape[1] - 1):
+        names.append("dim{}".format(i + 1))
+    dataframe.columns = names
+    return dataframe
+
+
+def pandasPlotter(filename):
+    dataframe = pandasReader(filename)
+    dataframe['Colour'] = dataframe['Colour'].apply(lambda a: 'r' if a == -1 else 'b')
+    return dataframe
+
+
+"""
+returns distance from points without using the "colour flag"
+"""
+
+
+def distance(p1, p2):
+    usefulp1 = p1[1:]
+    usefulp2 = p2[1:]
+    return np.linalg.norm(usefulp1 - usefulp2)
+
+
+import time
+
+t1 = time.time()
+
+
+def knn(point, data, k):
+    n, dim = data.shape
+    distances = np.zeros(n)
+    for key, entry in enumerate(data):
+        distances[key] = distance(entry, point)
+    b = np.zeros((n, data.shape[1] + 1))
+    b[:, :-1] = data
+    b[:, -1] = distances
+    b = b[b[:, -1].argsort(kind='mergesort')]
+    return b[:k, :-1], b[:k, -1]
+
+
+def sign(neighbours):
+    signum = sum(neighbours[0][:, 0])
+    return 1 if signum == 0 else signum/abs(signum)
+
+
+def randomlySplitData(data, l):
+    n = len(data)
+    copyData = data.copy()
+    np.random.shuffle(copyData)
+    returnList = [None] * l
+    for i in range(l):
+        returnList[i] = copyData[n * i // l:n * (i + 1) // l]
+    return returnList
+
+
+def createD_i(listOfData, i):
+    dim = len(listOfData[0][0])
+    print(dim)
+    returnArray = np.ndarray((0, dim))
+    for k in range(len(listOfData)):
+        if k != i:
+            returnArray = np.concatenate((returnArray, listOfData[k]))
+
+    return returnArray, listOfData[i]
+
+
+def classifyPoint(point, data, k):
+    kNN = knn(point, data, k)
+    classification = sign(kNN)
+    return point, classification
+
+
+data = numpyReader('bananas-1-2d.train.csv')
+#plotData = pandasPlotter('bananas-1-2d.train.csv')
+#plotData.plot.scatter(x='dim1', y='dim2', c='Colour')
+#plt.show()
+point = (1, 0.823, 0.495)
+# data = numpyReader('bananas-1-4d.train.csv')
+# data = numpyReader('toy-10d.train.csv')
+
+split = randomlySplitData(data, 5)
+D_i = createD_i(split, 0)
+print(classifyPoint(point, data, 50))
+kRange = np.arange(1, 101)
+
+t2 = time.time()
+print(t2 - t1)
--- kdtree.py
+++ kdtree.py
@@ -13,10 +13,17 @@ def intersects(point, radius, bb):
     return any(maxIntersect) or any(minIntersect)
 
 
+'''
+this was so so hard to program and yet it is so bad that it actually takes longer than a simple bruteforce sort and search :))
+
+builds a kdTree out of our data so we can search a LOT faster: O(log(n)) instead of O(n)
+'''
+
+
 class kdTree:
     # inner class node
     class Node:
-        def __init__(self, bb, currentAxis, parent=None):
+        def __init__(self, bb, currentAxis, parent):
             self.loc = None  # location for inner nodes
             self.right = None  # subtree
             self.left = None  # subtree
@@ -30,11 +37,14 @@ class kdTree:
 
     def __init__(self, data, leafsize):
         copydata = np.copy(data)
-        self.kdNode = self.buildTree(copydata, leafsize=leafsize)
+        self.bb = self.computeBB(data)
+        self.kdNode = self.buildTree(copydata, self.bb, parent=self, leafsize=leafsize)
         self.left = self.kdNode.left
         self.right = self.kdNode.right
+        self.data = data
+        self.isLeaf = lambda: False  # just to catch some errors that won't exist outside of testing stuff that's not necessary
 
-    def buildTree(self, data, depth=0, leafsize=10, parent=None):
+    def buildTree(self, data, bb, parent, depth=0, leafsize=10):
         n = data.shape[0]
         half = n // 2
         if n == 0:
@@ -44,36 +54,36 @@ class kdTree:
         axis = depth % dim + 1  # cycle through dimensions
 
         if n == 1:
-            bb = self.computeBB(data)
-            node = self.Node(bb, axis, None)
+            node = self.Node(bb, axis, parent)
             node.data = data
-            print(bb)
             return node
         # sort along given axis with mergesort in O(nlogn)
         # + 1 is because of "colour"
         sortedData = data[data[:, axis].argsort(kind='mergesort')]
         splitPoint = sortedData[half]
 
+        leftBB = bb.copy()
+        rightBB = bb.copy()
+        leftBB[1] = splitPoint[1:]
+        rightBB[0] = splitPoint[1:]
         # create next node
-        boundingBox = self.computeBB(data)
-        node = self.Node(bb=boundingBox, currentAxis=axis, parent=parent)
+        node = self.Node(parent=parent, bb=bb, currentAxis=axis)
         node.loc = splitPoint
 
         # we're using this split either way
         leftHalf = sortedData[:half]
         rightHalf = sortedData[half:]
+
         # if still too big -> split again
         if half >= leafsize:
-            node.left = self.buildTree(leftHalf, depth=depth + 1, leafsize=leafsize, parent=self)
-            node.right = self.buildTree(rightHalf, depth=depth + 1, leafsize=leafsize, parent=self)
+            node.left = self.buildTree(leftHalf, bb=leftBB, parent=node, depth=depth + 1, leafsize=leafsize)
+            node.right = self.buildTree(rightHalf, bb=rightBB, parent=node, depth=depth + 1, leafsize=leafsize)
         # else create left and right with data
         else:
-            BB1 = self.computeBB(leftHalf)
-            node.left = self.Node(bb=BB1, currentAxis=axis, parent=self)
+            node.left = self.Node(bb=leftBB, currentAxis=axis, parent=node)
             node.left.data = leftHalf
 
-            BB2 = self.computeBB(rightHalf)
-            node.right = self.Node(bb=BB2, currentAxis=axis, parent=self)
+            node.right = self.Node(bb=rightBB, currentAxis=axis, parent=node)
             node.right.data = rightHalf
         return node
 
@@ -85,12 +95,31 @@ class kdTree:
         return bb
 
 
+'''
+finds all leaves under a given node, very similar to inorder traversal
+'''
+
+
+def findAllLeaves(pivot, radius, node, foundData=[]):
+    if node.left is not None:
+        findAllLeaves(pivot=pivot, radius=radius, node=node.left, foundData=foundData)
+    if node.isLeaf() & intersects(pivot, radius, node.bb):
+        for dp in node.data:
+            foundData.append(dp)
+    if node.right is not None:
+        findAllLeaves(pivot=pivot, radius=radius, node=node.right, foundData=foundData)
+    return np.asarray(foundData)
+
+
+'''
+nearest neighbour search in O(log(n)) time using our kdtree
+'''
+
+
 def nn(node, pivot):
     # go to the leaf where the pivot would be
     while not node.isLeaf():
         axis = node.currentAxis
-        print("Axis:", axis)
-        print(pivot[axis])
         if pivot[axis] < node.loc[axis]:
             return nn(node.left, pivot)
         else:
@@ -106,25 +135,105 @@ def nn(node, pivot):
         if dist < mindist:
             mindist = dist
             minPoint = dataPoint
+
+    # if hypersphere around minpoint intersects neighbouring leafs -> there could be closer points
+    # -> go up to parent enough times, so we check all neighbouring leaves
     if intersects(minPoint, mindist, node.bb):
-        print(intersects(minPoint, mindist, node.bb))
-    # todo: if intersects: check neighbouring leafs
+        # jump once for leaving the leaf,
+        # then 2**(dim+1) == 2** shape[0] to reach last common (grandgrand..)parent of all neighbours
+        jumps = 2 ** (minPoint.shape[0]) + 1
+        parentNode = node
+        for i in range(jumps):
+            # if check just to catch errors in testing, not really needed
+            if isinstance(parentNode, kdTree.Node):
+                parentNode = parentNode.parent
+        # append all neighbours
+        dataList = findAllLeaves(parentNode)
+        # search all those neighbours
+        for dataPoint in dataList:
+
+            dist = distance(dataPoint, pivot)
+            # if smaller dist ->  replace
+            if dist < mindist:
+                mindist = dist
+                minPoint = dataPoint
     return minPoint, mindist
 
 
-def knn(self, point, k):
-    # todo everything
-    pass
+'''
+reaally similar to nn 
+'''
 
 
+def knn(node, pivot, k):
+    dim = len(pivot) - 1
+    # go to the leaf where the pivot would be
+    while not node.isLeaf():
+        axis = node.currentAxis
+        if pivot[axis] < node.loc[axis]:
+            return knn(node.left, pivot, k)
+        else:
+            return knn(node.right, pivot, k)
+
+    # init kmindist as inf so all other points are closer
+    kmindist = float('inf')
+    # init minArray with correct size so we dont have to copy all the time
+    # and with infinity distances so they all get replaced
+    minArray = np.full((k, 2), np.inf, dtype=object)
+
+    # check for closest point in leaf
+    for dataPoint in node.data:
+        dist = distance(dataPoint, pivot)
+        if dist < kmindist:
+            minArray[k - 1] = np.asmatrix((dataPoint, dist))
+            # sort so we just have to replace last point
+            minArray = minArray[minArray[:, 1].argsort(kind='mergesort')]
+            kmindist = minArray[-1, 1]
+
+    # if hypersphere around minpoint intersects neighbouring leafs -> there could be closer points
+    # -> go up to parent enough times, so we check all neighbouring leaves
+    parentNode = node.parent
+    i = 0
+    print(minArray[-1])
+    while isinstance(parentNode, kdTree.Node) & intersects(pivot, kmindist, parentNode.bb):
+        # while distance(pivot, parentNode.loc) < kmindist:
+        parentNode = parentNode.parent
+    # append all neighbours
+    print("now finding all leaves")
+    dataList = findAllLeaves(pivot=pivot, radius=kmindist, node=parentNode)
+    # search all those neighbours
+    print(len(dataList))
+    for dataPoint in dataList:
+        dist = distance(dataPoint, pivot)
+        if dist < kmindist:
+            minArray[k - 1] = [dataPoint, dist]
+            # sort so we just have to replace last point
+            minArray = minArray[minArray[:, 1].argsort(kind='mergesort')]
+            kmindist = minArray[-1, 1]
+    return minArray
+
+
+t1 = time.time()
 data = numpyReader('bananas-1-2d.train.csv')
 points = data.copy()
-head = kdTree(points, leafsize=50).kdNode
-x = points[:, 1],
+head = kdTree(points, leafsize=64).kdNode
+x = points[:, 1]
 y = points[:, 2]
 
-point = np.asarray((1, .05645, 0.6473))
-
-print(nn(head, point))
+point = (1, 0.5, 0.3)
+t3 = time.time()
+dah = knn(head, point, 20)
+print(dah[:, 1:])
+t4 = time.time()
 plt.scatter(x=x, y=y, s=1)
+for entry in dah[:, :1]:
+    plt.scatter(entry[0][1:][0], entry[0][1:][1], c='red')
+
+t2 = time.time()
+
+print("alles insgesamt hat {} sekunden gebraucht".format(t2 - t1))
+print("und die knn suche hat {} sekunden gedauert".format(t4 - t3))
+plt.scatter(point[1], point[2], c='g')
+plt.xlim(point[1] - 0.1, point[1] + 0.1)
+plt.ylim(point[2] - 0.1, point[2] + 0.1)
 plt.show()
