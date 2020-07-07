d1c241d922108904c7f535f22c7887963d088232
uelgerni <https://uelgerni@github.com>
2020-07-07 22:07:06 +0200

    added a test

--- main.py
+++ main.py
@@ -16,30 +16,38 @@ tree = kdTree(data)
 '''
 just some tests
 '''
-for i in range(100):
-    rando = np.random.randint(0, 5000)
-    p = np.array(data[rando])
-    classifiedPoint = classifyPoint(p, tree, 5)
 
-    plotData = pandasPlotter(filename)
-    # print(classiefiedPoints)
-    # print(dataBeautifier(classiefiedPoints)[0])
+'''
 
-    pNearest = dataBeautifier(knn(tree, p, 5))[0]
-    xVals = pNearest[:, 1]
-    yVals = pNearest[:, 2]
-    cmap = ['black' for val in pNearest[:, 0]]
-    cmap2 = ['r' if val == -1 else 'blue' for val in pNearest[:, 0]]  # else 'b']
+'''
+def test1():
+    for i in range(100):
+        rando = np.random.randint(0, 5000)
+        p = np.array(data[rando])
+        classifiedPoint = classifyPoint(p, tree, 5)
 
-    plotData.plot.scatter(x='dim1', y='dim2', c='Colour', s=15)
-    plt.xlim((p[1] - .03, p[1] + .03))
-    plt.ylim((p[2] - .03, p[2] + .03))
-    pcol = 'r' if p[0] == -1 else 'blue'
-    plt.scatter(x=p[1], y=p[2], c=pcol, s=100)
-    pguessedcol = 'r' if sign(dataBeautifier(knn(tree, p, 5))[0]) == -1 else 'blue'
-    plt.scatter(x=p[1], y=p[2], c=pcol, s=10)
+        plotData = pandasPlotter(filename)
+        # print(classiefiedPoints)
+        # print(dataBeautifier(classiefiedPoints)[0])
 
-    plt.scatter(x=xVals, y=yVals, c=cmap, s=50)
-    plt.scatter(x=xVals, y=yVals, c=cmap2, s=10)
-    plt.savefig('pictures/{}'.format(i))
-    plt.close()
+        pNearest = dataBeautifier(knn(tree, p, 5))[0]
+        xVals = pNearest[:, 1]
+        yVals = pNearest[:, 2]
+        cmap = ['black' for val in pNearest[:, 0]]
+        cmap2 = ['r' if val == -1 else 'blue' for val in pNearest[:, 0]]  # else 'b']
+
+        plotData.plot.scatter(x='dim1', y='dim2', c='Colour', s=15)
+        plt.xlim((p[1] - .03, p[1] + .03))
+        plt.ylim((p[2] - .03, p[2] + .03))
+        pcol = 'r' if p[0] == -1 else 'blue'
+        plt.scatter(x=p[1], y=p[2], c=pcol, s=100)
+        pguessedcol = 'r' if sign(dataBeautifier(knn(tree, p, 5))[0]) == -1 else 'blue'
+        plt.scatter(x=p[1], y=p[2], c=pcol, s=10)
+
+        plt.scatter(x=xVals, y=yVals, c=cmap, s=50)
+        plt.scatter(x=xVals, y=yVals, c=cmap2, s=10)
+        plt.savefig('pictures/{}'.format(i))
+        plt.close()
+
+
+#test1()
